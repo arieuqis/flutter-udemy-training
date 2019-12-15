3acdac01024f1b69001fb79f8a0da5ebeadaf43e
arieuqis <siqueira.bcl@gmail.com>
2019-12-15 13:32:38 -0300

    Tela de produtos concluída

--- loja_virtual/lib/screens/product_screen.dart
+++ loja_virtual/lib/screens/product_screen.dart
@@ -1,8 +1,8 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
 class ProductScreen extends StatefulWidget {
-
   final ProductData product;
 
   ProductScreen(this.product);
@@ -12,15 +12,126 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
-
   final ProductData product;
+
+  String size;
 
   _ProductScreenState(this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+                images: product.images.map((url) {
+                  return NetworkImage(url);
+                }).toList(),
+                dotSize: 4.0,
+                dotSpacing: 15.0,
+                dotBgColor: Colors.transparent,
+                dotColor: primaryColor,
+                autoplay: false),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ),
+                _getDefaultSpacing(),
+                Text(
+                  "Tamanho",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 34.0,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.5,
+                    ),
+                    children: product.sizes.map((tamanho) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            size = tamanho;
+                          });
+                        },
+                        child: Container(
+                          width: 50.0,
+                          alignment: Alignment.center,
+                          child: Text(tamanho),
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4.0)),
+                              border: Border.all(
+                                  color: tamanho == size
+                                      ? primaryColor
+                                      : Colors.grey[500],
+                                  width: 3.0)),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                _getDefaultSpacing(),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    onPressed: size != null ? () {} : null,
+                    color: primaryColor,
+                    child: Text(
+                      "Adicionar ao carrinho",
+                      style: TextStyle(fontSize: 18.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+                _getDefaultSpacing(),
+                Text(
+                  "Descrição",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16.0),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _getDefaultSpacing() {
+    return SizedBox(
+      height: 16.0,
     );
   }
 }
